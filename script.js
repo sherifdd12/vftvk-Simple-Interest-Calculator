436adb766a21087703ece19493a000581a78b58d
sherif mohamed <sherware_dd@hotmail.com>
2021-04-30 04:41:11 +0300

    Add existing file

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function compute()
      var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
       var year = new Date().getFullYear()+parseInt(years);
-      document.getElementById("result").innerText="If you deposit"+" "+principal+" "+ ",at an interest rate of"+" "+rate+ "%.You will receive an amount of"+" "+interest+" "+",in the year"+" "+year
+      document.getElementById("result").innerText="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
 }
      function updateRate() 
 {
